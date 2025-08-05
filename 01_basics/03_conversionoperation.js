b6cd5f55b08802f80e1734cf8a2ffd58a04fd763
Deepanshu <117531973+Deepanshu737374@users.noreply.github.com>
2025-08-05 18:34:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "33a"
+
+console.log(typeof score)
+console.log((typeof(score)));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+//"33" =>33
+//"33abc" =>NaN
+//true => 1; false =>0
+
+let isLoggedIn = ""
+
+let booleanisloggedIn = Boolean(isLoggedIn)
+console.log(booleanisloggedIn)
+
+//1 =>true; 0=> false
+//"" =>false
+//"deep" =>true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(typeof stringnumber);
