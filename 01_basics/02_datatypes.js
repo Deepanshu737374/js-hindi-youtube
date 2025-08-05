0b9401afdcbb4000e640b4b245384128e4c3076f
Deepanshu <117531973+Deepanshu737374@users.noreply.github.com>
2025-08-05 17:57:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all JS code as a newer version
+
+
+//alert(3+3)   //we are using nodejs, not browser
+
+console.log(3 
+        +
+        3)  //code readability should be high
+
+let name = "Deepanshu gupta"
+let age = 20;
+let isLoggedIn= false
+
+//number =>2 to power53
+//bigint
+//string => ""
+//boolean =>  true/false
+//null =>standalone value
+//undefined =>undefined
+//symbol =>unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
